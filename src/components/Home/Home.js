944d217b1f5d32bae013f66c73fb58361b4961eb
pras75299 <singh.prashantking@gmail.com>
2021-05-27 17:31:05 +0530

    add smooth scroll to navbar and add banner with icons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,27 @@
 import React from "react";
-const Home = () => {
-    return <section className="homeSection" id="home">
-      <h1>Home Page</h1>
-    </section>;
-  };
-
-  export default Home;
+import classes from './Home.module.css';
+import { Link } from 'react-scroll'
+import { FaGithub, FaLinkedinIn, FaAngleDoubleDown } from 'react-icons/fa';
+export default function Home() {
+  return (<>
+    <section className={classes.introSection} id="home">
+        <div className={classes.bgCms}></div>
+		<div className={classes.introInner}>
+			<div className={classes.introContent}>
+				<h2><span>I'm</span> Prashant Singh</h2>
+				<p>Web Designer . Front-End Developer</p>
+				<div className={classes.socialLinks}>
+					<a href="#" className={classes.facebook}><FaLinkedinIn/></a>
+					<a href="#" className={classes.twitter}><FaGithub/></a>
+				</div>
+				<div className={classes.downSection}>
+                    <Link activeClass="active" spy={true} smooth={true} to='about' offset={200}>
+                        <FaAngleDoubleDown className={classes.bounce}/>
+                    </Link>					
+			    </div>
+		    </div>
+		</div>
+	</section>
+    </>
+  );
+}
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from './Nav.module.css';
 import { AiOutlineBars } from 'react-icons/ai';
 import { Link } from 'react-scroll'
 
 function Nav() {
+  useEffect(() => {
+    const header = document.getElementById("myHeader");
+    const sticky = header.offsetTop;
+    const scrollCallBack = window.addEventListener("scroll", () => {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", scrollCallBack);
+    };
+  }, []);
   return (
-    <header className={classes.headerSection}>
+    <header id="myHeader" className={classes.headerSection}>
       <div className={classes.logo}>
         <Link activeClass="active" spy={true} smooth={true} to='home'>PRASHANT</Link>
       </div>
