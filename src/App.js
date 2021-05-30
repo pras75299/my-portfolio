cc59cdaad3ed9b585e12e21fa6d5bf4ab09926ab
pras75299 <singh.prashantking@gmail.com>
2021-05-30 22:14:53 +0530

    add react-bootstarp, add about section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import About from "./components/About/About";
 import Nav from "./components/Nav/Nav";
 import Home from "./components/Home/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App() {
   return (
     <>
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,29 @@
 import React from "react";
+import {Container, Row, Col} from 'react-bootstrap';
+
+import profileImag from '../../images/profile.jpg';
 
 export default function About() {
   return (
-    <section className="homeSection" id="about">
-      <h1 className="commontitleclass">About page</h1>
-      <br />
-      <p>This is a website using different apis to show information about Cats, Quotes and Harry potter characters.</p>
+    <section className="aboutSection fix" id="about">
+      <Container className="pt100 pb100">
+        <Row>
+          <div className="sectionTitle mb70">
+            <h2>About Me</h2>
+          </div>
+          <Col xs={6} md={6} className="aboutImg">
+            <img src={profileImag} alt=""/>
+          </Col>
+          <Col xs={6} md={6} className="aboutText">
+            <p>I am <ins>Prashant Singh.</ins> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text making it over 2000 years old.</p>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+
+            <button type="button" className="btn siteBtn btnOutline transitionEase mt10">Download CV</button>
+          </Col>
+        </Row>
+      </Container>      
     </section>
+    
   );
 }
